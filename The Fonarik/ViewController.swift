01c7899ad8931c3bf4020bcda5c3c988d540dd4a
gulyaeve <gulyaev.e@icloud.com>
2020-12-10 21:01:29 +0300

    Added "Traffic lights" mode.

--- The Fonarik/ViewController.swift
+++ The Fonarik/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var isLight = false
+    var isLight = 0
 
     override var prefersStatusBarHidden: Bool{
         return true
@@ -22,13 +22,43 @@ class ViewController: UIViewController {
         updateUI()
         
     }
+    /// Switch to color
+    /// - returns: UIColor
+    func lightOn(number: Int) -> UIColor {
+        switch number {
+        case 0:
+            let color = UIColor.black
+            return color
+        case 1:
+            let color = UIColor.red
+            return color
+        case 2:
+            let color = UIColor.yellow
+            return color
+        case 3:
+            let color = UIColor.green
+            return color
+        default:
+            let color = UIColor.white
+            return color
+        }
+    }
     fileprivate func updateUI() {
-        view.backgroundColor = isLight ? .white: .black
-        UIScreen.main.brightness = isLight ? CGFloat(1.0): CGFloat(0.0)
+        view.backgroundColor = lightOn(number: isLight)
+        // Set screen brightness: ON for colors, OFF for black
+        if isLight == 0 {
+            UIScreen.main.brightness = CGFloat(0.0)
+        } else {
+            UIScreen.main.brightness = CGFloat(1.0)
+        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        isLight.toggle()
+        if isLight > 3 {
+            isLight = 0
+        } else {
+            isLight += 1
+        }
         updateUI()
     }
     
